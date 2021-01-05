a3d71620b64cad0417e919b6b58b757f6e4fed08
mjones1818 <michael.jones0532@gmail.com>
2021-01-05 09:08:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (names, event) {
+  let results = []
+  for (let x = 0; x < names.length; x++) {
+    results.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+  }
+  return results
+}
+
+function countDown (number) {
+  while (number >=0) {
+    console.log(number)
+    number--
+  }
+}
